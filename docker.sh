f57e5e31cd9986f905e93a32f00949ae4a77a773
pardha <pardha@pardha.com>
2017-04-27 15:32:31 -0400

    docker.sh changed for node

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
-docker build -t="httpd_server" .
-docker run -d --name mount_git -v /root/sample:/usr/local/apache2/htdocs httpd_server
+/usr/bin/docker build -t="httpd_server" /root/sample/.
+/usr/bin/docker run -d --name mount_git -p 80:80 -v /root/sample:/usr/local/apache2/htdocs httpd_server
